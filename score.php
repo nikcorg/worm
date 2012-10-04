e11ab54bb634b002560fb9e9e8c021cdd864f693
Niklas Lindgren <niklas.lindgren@luxus.fi>
2012-10-04 13:55:27 +0300

    refactoring

--- score.php
+++ score.php
@@ -4,22 +4,22 @@ define("OUTFILE", __DIR__ . "/.scores");
 define("SNAPS_FS", __DIR__ . "/snaps");
 define("SNAPS_WEB", "snaps");
 $scores = file(OUTFILE, FILE_IGNORE_NEW_LINES|FILE_SKIP_EMPTY_LINES);
-function checksum_is_valid($cs) {
-    $check = substr($cs, -1, 1);
-    $rest = preg_split("//", substr($cs, 0, -1), -1, PREG_SPLIT_NO_EMPTY);
-    $sum = array_sum($rest);
-    $ret = substr($sum, -1, 1) === $check;
-    return $ret;
+function strtoarr($str) {
+    return preg_split("//", $str, -1, PREG_SPLIT_NO_EMPTY);
 }
 function checksum($str) {
     $i = 0;
     $l = strlen($str);
-    $cs = 0;
-    for (; $i < $l; $i++) {
-        $cs += ord($str{$i});
-    }
-    $cs .= substr(array_sum(preg_split("//", $cs, -1, PREG_SPLIT_NO_EMPTY)), -1, 1);
+    $cs = array_sum(array_map("ord", strtoarr($str)));
+    $check = array_sum(strtoarr($cs));
+    $cs .= substr($check, -1, 1);
     return intval($cs);
+}
+function checksum_is_valid($cs) {
+    $check = substr($cs, -1, 1);
+    $rest = strtoarr(substr($cs, 0, -1));
+    $sum = array_sum($rest);
+    return substr($sum, -1, 1) === $check;
 }
 function require_in($cn, $vars) {
     foreach ($vars as $nm) {
